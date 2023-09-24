8cc0b2c5dba874dd3bd71d15e1ff048c000647de
xpartla <xpartla@stuba.sk>
2023-09-24 21:25:19 +0200

    Difference between IEEE 802.3 RAW / IEEE 802.3 LLC / IEEE 802.3 LLC and SNAP Also has a base for SAP difference

--- using_scapy.py
+++ using_scapy.py
@@ -16,14 +16,20 @@ def extract_8023_type(raw_packet):
     if len(raw_packet) >= 3:
         control = int.from_bytes(raw_packet[2:3], byteorder='big')
 
-        if control == 0xaaaa03:
+        if control == 0xaaaa03:     # IEEE 802.3 LLC and SNAP
             return "IEEE 802.3 LLC and SNAP"
-        elif control == 0xaaaa:
+        elif control == 0xaaaa:     # IEEE 802.3 LLC
             return "IEEE 802.3 LLC"
         else:
             return "IEEE 802.3 RAW"
     else:
         return None
+
+
+def extract_sap(raw_packet):
+    llc_header = raw_packet[3:8]
+    sap = int.from_bytes(llc_header[:3], byteorder='big')
+    return sap
 
 
 def print_pcap_hex(filename):
@@ -42,6 +48,7 @@ def print_pcap_hex(filename):
                 if eth_type <= 1500:
                     eth_length = 2
                     packet_type = extract_8023_type(bytes(packet))
+                    sap_name = get_sap_name(extract_sap(bytes(packet)))
                 else:
                     eth_length = 14
                     packet_type = "Ethernet II"
@@ -55,6 +62,8 @@ def print_pcap_hex(filename):
             # Print the packet information
             print(f"Packet {packet_count + 1}")
             print(f"Packet type: {packet_type}")
+            if packet_type == "IEEE 802.3 LLC and SNAP" or "IEEE 802.3 LLC":
+                print(f"SAP: {sap_name}")
             print(f"API Length: {len(packet)} bytes")
             print(f"Over medium Length: {len(packet) + eth_length} bytes")
             print(f"Packet:\n{hex_data}\n")
@@ -64,6 +73,16 @@ def print_pcap_hex(filename):
         print(f"File not found: {filename}")
     except Exception as e:
         print(f"Error reading the pcap file: {e}")
+
+
+def get_sap_name(sap):
+    #mapping SAP names
+    sap_mappings = {
+        0x42: "ARP",
+        0x4C: "NetBIOS",
+        #ADD MORE
+    }
+    return sap_mappings.get(sap)
 
 
 if __name__ == "__main__":
