0c38aba82f045fc57a2d4e77da9c2b1288b887fc
xpartla <xpartla@stuba.sk>
2023-09-24 16:32:23 +0200

    switched to scapy instead of dpkt, not sure if I could use dpkt for the project

--- using_scapy.py
+++ using_scapy.py
@@ -11,8 +11,25 @@ def print_pcap_hex(filename):
             # Convert packet data to hexadecimal format
             hex_data = ' '.join(['{:02X}'.format(byte) for byte in bytes(packet)])
 
+            #Determine packet type
+            packet_type = None
+            if packet.haslayer("Dot3"):
+                packet_type = packet["Dot3"].len
+            elif packet.haslayer("SNAP"):
+                packet_type = len(packet["SNAP"])
+
+            # Ethernet header Length
+            eth_length = 14
+
+            if packet_type == 0x0000:
+                #Novell 802.3 RAW -> +3 (Control field)
+                eth_length = 17
+            elif packet_type == 0x800:
+                #IEEE 802.3 LLC / IEEE 802.3 LLC + SNAP
+                eth_length = 22 if packet.haslayer("SNAP") else 18
+
             # Print the hexadecimal data to the console
-            print(f"Packet {packet_count + 1} (Length: {len(packet)} bytes):\n{hex_data}\n")
+            print(f"Packet {packet_count + 1} \n(API Length: {len(packet)} bytes)\n(Over medium Length: {len(packet) + eth_length} bytes) \nPacket:\n{hex_data}\n")
             packet_count += 1
 
     except FileNotFoundError:
@@ -22,5 +39,5 @@ def print_pcap_hex(filename):
 
 
 if __name__ == "__main__":
-    pcap_filename = "eth-1.pcap"
+    pcap_filename = "trace-2.pcap"
     print_pcap_hex(pcap_filename)
