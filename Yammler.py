39356a699d4abf602a0b82eb5d7a63d542151cc0
xpartla <xpartla@stuba.sk>
2023-10-09 20:21:08 +0200

    2. a) ARP, IPV4, IPV6 -done    b) pre arp, ivp4/v6 -done    c) done    d) pre udp pri ipv4 done

--- Yammler.py
+++ Yammler.py
@@ -1,3 +1,5 @@
+from binascii import hexlify
+
 from scapy.all import rdpcap
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import LiteralScalarString
@@ -56,10 +58,68 @@ def extract_mac(raw_packet):
     else:
         return None, None
 
+def extract_arp_ip(raw_packet):
+    if len(raw_packet) >=42:
+        s = '.'.join(map(str, raw_packet[28:32]))
+        d = '.'.join(map(str, raw_packet[38:42]))
+        #print(s) src
+        #print(d) dst
+        return s, d
+    else:
+        return None, None
+def extract_ipv4_ip(raw_packet):
+    if len(raw_packet) >= 34:
+        s = '.'.join(map(str, raw_packet[26:30]))
+        d = '.'.join(map(str, raw_packet[30:34]))
+        return s, d
+    else:
+        return None, None
+
+def extract_ipv6_ip(raw_packet):
+    if len(raw_packet) >= 54:
+        s = format_mac(raw_packet[22:38])
+        d = format_mac(raw_packet[38:54])
+        return s,d
+    else:
+        return None,None
+
+def extract_ipv4_protocol(raw_packet):
+    if len(raw_packet) >= 24:
+        return int.from_bytes(raw_packet[23:24], byteorder='big')
+
+def extract_UDP_protocols(raw_packet):
+    if len(raw_packet) >= 38:
+        return int.from_bytes(raw_packet[34:36], byteorder='big'),int.from_bytes(raw_packet[36:38], byteorder='big')
+    else:
+        return None
 
 def main():
     pcap_filename = "trace-27.pcap"
     packets_data = []
+
+    ethertype_data = {}
+    with open("Ethertype_data.txt", "r") as ethertype_file:
+        for line in ethertype_file:
+            parts = line.strip().split(":")
+            if len(parts) == 2:
+                ethertype, protocol = int(parts[0]), parts[1]
+                ethertype_data[ethertype] = protocol
+
+    udp_well_known_data = {}
+    with open("Udp_protocol_data.txt", "r") as udp_file:
+        for line in udp_file:
+            parts = line.strip().split(":")
+            if len(parts) == 2:
+                udp_prot, well_known_protocol = int(parts[0]), parts[1]
+                udp_well_known_data[udp_prot] = well_known_protocol
+
+    ipv4_protocol_data = {}
+    with open("Ipv4_protocol_data.txt", "r") as ipv4_file:
+        for line in ipv4_file:
+            parts = line.strip().split(":")
+            if len(parts) == 2:
+                ipv4_prot, protocol = int(parts[0]), parts[1]
+                ipv4_protocol_data[ipv4_prot] = protocol
 
     try:
         packets = rdpcap(pcap_filename)
@@ -68,10 +128,18 @@ def main():
             eth_length = 0
             packet_type = "Unknown"
             sap_name = None
-            ethertype_name = None
+            pid_name = None
+            e2_protocol = None
+            src_ip = None
+            dst_ip = None
+            well_known_src = None
+            well_known_dst = None
+            ipv4_protocol = None
             dest, src = extract_mac(bytes(packet))
-
+            ipv4_type = extract_ipv4_protocol(bytes(packet))
             eth_type = extract_ethertype(bytes(packet))
+            udp_src, udp_dest = extract_UDP_protocols(bytes(packet))
+            print(udp_dest)
 
             if eth_type is not None:
                 if eth_type <= 1500:
@@ -80,10 +148,34 @@ def main():
                     if packet_type == "IEEE 802.3 LLC" or packet_type == "IEEE 802.3 LLC & SNAP":
                         sap_name = get_sap_name(extract_sap(bytes(packet)))
                     if packet_type == "IEEE 802.3 LLC & SNAP":
-                        ethertype_name = get_ethertype_name(extract_ether(bytes(packet)))
+                        pid_name = get_pid_name(extract_ether(bytes(packet)))
                 else:
                     eth_length = 4
                     packet_type = "ETHERNET II"
+                    if eth_type in ethertype_data:
+                        e2_protocol = ethertype_data[eth_type]
+
+                        if eth_type == 2054:  # ARP
+                            src_ip, dst_ip = extract_arp_ip(bytes(packet))
+                            #print(src_ip)
+
+                        if eth_type == 2048: #IPV4
+                            src_ip, dst_ip = extract_ipv4_ip(bytes(packet))
+
+                            if ipv4_type in ipv4_protocol_data:
+                                ipv4_protocol = ipv4_protocol_data[ipv4_type]
+
+                            if udp_src in udp_well_known_data:
+                                well_known_src = udp_well_known_data[udp_src]
+                            if udp_dest in udp_well_known_data:
+                                well_known_dst = udp_well_known_data[udp_dest]
+
+
+                        if eth_type == 34525: #IPV6
+                            src_ip, dst_ip = extract_ipv6_ip(bytes(packet))
+
+
+
             else:
                 packet_type = "Unknown"
 
@@ -105,11 +197,31 @@ def main():
                 "hexa_frame": hexa_frame_literal,
             }
 
-            if sap_name and ethertype_name == None:
+            if sap_name and pid_name == None:
                 packet_data["sap"] = sap_name
 
-            if ethertype_name:
-                packet_data["pid"] = ethertype_name
+            if pid_name:
+                packet_data["pid"] = pid_name
+
+            if e2_protocol:
+                packet_data["vnoreny protokol (2a)"] = e2_protocol
+
+            if eth_type == 2054 or eth_type == 2048 or eth_type == 34525: #ARP, IPV4, IPV6
+                packet_data["src_ip"] = src_ip
+                packet_data["dst_ip"] = dst_ip
+
+            if eth_type == 2048:
+                if ipv4_protocol:
+                    packet_data["IpV4 protocol"] = ipv4_protocol
+                    print(ipv4_protocol)
+
+                packet_data["udp_src"] = udp_src
+                packet_data["udp_dest"] = udp_dest
+                if well_known_src:
+                    packet_data["udp src well known port"] = well_known_src
+                if well_known_dst:
+                    packet_data["udp dst well known port"] = well_known_dst
+
 
             packets_data.append(packet_data)
 
@@ -127,7 +239,7 @@ def main():
     yaml = YAML()
     yaml.indent(offset=2, sequence=4)
 
-    with open("output.yaml", "w") as yaml_file:
+    with open("output25.yaml", "w") as yaml_file:
         yaml.dump(yaml_data, yaml_file)
 
 
@@ -153,8 +265,8 @@ def get_sap_name(sap):
     return sap_mappings.get(sap)
 
 
-def get_ethertype_name(ethertype):
-    ethertype_mappings = {
+def get_pid_name(pid):
+    pid_mappings = {
         0x200: "XEROX PUP",
         0x2000: "CDP",
         0x2004: "DTP",
@@ -176,7 +288,7 @@ def get_ethertype_name(ethertype):
         0X8863: "PPPoE Discovery Stage",
         0X8864: "PPoE Session Stage"
     }
-    return ethertype_mappings.get(ethertype)
+    return pid_mappings.get(pid)
 
 
 #if __name__ == "__main__":
